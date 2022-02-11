bcd5912093e1545b05adc048c0377fe8c2c26db7
mstream <maciej.laciak@gmail.com>
2022-02-11 06:42:41 +0000

    add containers ports printing

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash -e
 
+function list_docker_containers_loop() {
+  while true; do
+    printf "\n===\n" 
+    sleep 5
+    status=$(docker ps --format "table {{.Names}}\t{{.Ports}}")
+    printf "===\n" 
+  done;
+}
+
 docker build -t 'local/prometheus:1.0' .
 docker build -t 'local/service:1.0' service
+
+(sleep 5 && list_docker_containers_loop) &
+
 docker-compose up
